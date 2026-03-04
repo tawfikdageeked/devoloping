cda312d0b0543c033d700b2f873d61c4114278b0
tawfikdageeked <mahmoudauf74@gmail.com>
2026-03-04 20:11:07 +0000

    did some java shit

---
+++ java/Data_Types.java
@@ -0,0 +1,30 @@
+public class Data_Types
+{
+
+    public static void main(String[] args)
+    {
+
+        int x = 8; // integers : 4 Bytes (only use with clean divisons cuz programming language are not that smart)
+        byte y = 8; // bytes : smallers than intergers only 1 Byte
+        short z = 7; // between itegers and bytes 2 Bytes
+        long l = 4; // big integer 8 Bytes
+        double n = 7.0; // 8 bytes but high precision
+        float m = 5.4f; // 4 bytes high precision too
+
+        char s = 'A';  // characters Unicode (I have no idea how is type of data useful)
+
+        String c = "Hello, World!"; // Infamous string
+
+        double a = n+= 8.94099034;
+
+
+
+
+
+
+        
+
+
+    }
+
+}
---
+++ java/printing.java
@@ -0,0 +1,24 @@
+public class printing // every program in java is inside a class cuz java is mainly OOP language
+{                // so all the functions are used to make methods even the main fuctions is called main method
+// note : it is better to name the classes different names from the methods
+    public static void main(String[] args)
+    {
+
+        System.out.println("Hello, World!"); // prints lines and ends it
+        System.out.print("Hello, World!"); // prints lines without ending it
+
+    }
+}
+
+/* 
+some strings useful shit
+
+\n  --> end line
+
+\t  --> tab
+
+\" --> writes double quoatation
+
+\\  --> writes black slash
+
+*/
